d178dfff29b9bb81530b4a60592d71d15910cea4
Baptiste CHALEIL <bchaleil@e3r12p22.42.fr>
2016-11-23 15:39:45 +0100

    xo norme

--- src/App.js
+++ src/App.js
@@ -7,36 +7,36 @@ import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import {yellow800, yellow900, yellow300} from 'material-ui/styles/colors';
 
-
 const muiTheme = getMuiTheme({
-  palette: {
-    primary1Color: yellow800,
-    primary2Color: yellow900,
-    primary3Color: yellow300,
-  }
+	palette: {
+		primary1Color: yellow800,
+		primary2Color: yellow900,
+		primary3Color: yellow300
+	}
 });
 
-injectTapEventPlugin();
 const Tmp = () => null;
+
+injectTapEventPlugin();
 
 class App extends Component {
 	render() {
 		return (
-<MuiThemeProvider muiTheme={muiTheme}>
-			<Router history={browserHistory}>
-				<Route path="/" component={Tmp}>
-					<IndexRoute component={Tmp}/>
-					<Route path="user/:id" component={Tmp}/>
-					<Route path="profile" component={Tmp}/>
-					<Route path="movie/:id" component={Tmp}/>
-				</Route>
-				<Route path="login" component={Login}/>
-				<Route path="register" component={Tmp}/>
-				<Route path="forget" component={Tmp}/>
-				<Route path="reset/:token" component={Tmp}/>
-				<Route path="*" component={Tmp}/>
-			</Router>
-  </MuiThemeProvider>
+			<MuiThemeProvider muiTheme={muiTheme}>
+				<Router history={browserHistory}>
+					<Route path="/" component={Tmp}>
+						<IndexRoute component={Tmp}/>
+						<Route path="user/:id" component={Tmp}/>
+						<Route path="profile" component={Tmp}/>
+						<Route path="movie/:id" component={Tmp}/>
+					</Route>
+					<Route path="login" component={Login}/>
+					<Route path="register" component={Tmp}/>
+					<Route path="forget" component={Tmp}/>
+					<Route path="reset/:token" component={Tmp}/>
+					<Route path="*" component={Tmp}/>
+				</Router>
+			</MuiThemeProvider>
 		);
 	}
 }
--- src/Login.js
+++ src/Login.js
@@ -2,21 +2,19 @@ import React, {Component} from 'react';
 import {browserHistory} from 'react-router';
 import TextField from 'material-ui/TextField';
 
-
-
 class Login extends Component {
 	render() {
 		return (
 			<div>
 				<TextField
-				  floatingLabelText="Login"
-				/><br />
-				<br />
+					floatingLabelText="Login"
+					/><br/>
+				<br/>
 				<TextField
-				  hintText="Password Field"
-				  floatingLabelText="Password"
-				  type="password"
-				/><br />
+					hintText="Password Field"
+					floatingLabelText="Password"
+					type="password"
+					/><br/>
 			  </div>
 		);
 	}
