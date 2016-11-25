02534abb19878c3b223a47c175b1b4c2a46c7716
Baptiste CHALEIL <bchaleil@e3r11p14.42.fr>
2016-11-25 17:55:03 +0100

    Nav Done

--- src/logged/Nav.js
+++ src/logged/Nav.js
@@ -1,26 +1,69 @@
 import React, {Component} from 'react';
+import {browserHistory} from 'react-router';
 import SelectField from 'material-ui/SelectField';
 import MenuItem from 'material-ui/MenuItem';
+import FontIcon from 'material-ui/FontIcon';
+import {BottomNavigation, BottomNavigationItem} from 'material-ui/BottomNavigation';
+import Paper from 'material-ui/Paper';
+import IconLocationOn from 'material-ui/svg-icons/communication/location-on';
+
+const account = <FontIcon className="material-icons">account_circle</FontIcon>;
+const search = <FontIcon className="material-icons">search</FontIcon>;
+const exit = <FontIcon className="material-icons">exit_to_app</FontIcon>;
 
 class Nav extends Component {
 	constructor() {
 		super();
+		this.state = {
+			selectedIndex: 1
+		};
+
 		this.handleSelectLang = this.handleSelectLang.bind(this);
+		this.handleSelectNav = this.handleSelectNav.bind(this);
+	}
+	componentDidMount() {
+		const pathname = this.context.router.getCurrentLocation().pathname;
+
+		if (pathname === '/') {
+			this.setState({selectedIndex: 1});
+		} else if (pathname === '/profile') {
+			this.setState({selectedIndex: 2});
+		}
 	}
 	handleSelectLang(event, index, value) {
 		this.context.langChange(value);
+	}
+	handleSelectNav(index) {
+		this.setState({selectedIndex: index});
+		if (index === 1) {
+			browserHistory.push('/');
+		} else if (index === 2) {
+			browserHistory.push('/profile');
+		} else if (index === 3) {
+			browserHistory.push('/login');
+		}
 	}
 
 	render() {
 		const {messages, lang} = this.context;
 		return (
 			<div>
-				<div>{messages.login}</div>
-				<SelectField value={lang} onChange={this.handleSelectLang} autoWidth>
-					<MenuItem value={'en'} primaryText="English"/>
-					<MenuItem value={'fr'} primaryText="Français"/>
-					<MenuItem value={'es'} primaryText="Castellano"/>
-				</SelectField>
+				<Paper zDepth={1}>
+					<BottomNavigation selectedIndex={this.state.selectedIndex}>
+						<div></div>
+						<BottomNavigationItem label={messages.nav.search} icon={search} onTouchTap={() => this.handleSelectNav(1)}/>
+						<BottomNavigationItem label={messages.nav.profil} icon={account} onTouchTap={() => this.handleSelectNav(2)}/>
+						<BottomNavigationItem label={messages.nav.logout} icon={exit} onTouchTap={() => this.handleSelectNav(3)}/>
+						<div></div>
+						<div style={{flex: 'none'}}>
+							<SelectField value={lang} onChange={this.handleSelectLang} style={{width: '150px', marginRight: '20px'}}>
+								<MenuItem value={'en'} primaryText="English"/>
+								<MenuItem value={'fr'} primaryText="Français"/>
+								<MenuItem value={'es'} primaryText="Castellano"/>
+							</SelectField>
+						</div>
+					</BottomNavigation>
+				</Paper>
 			</div>
 		);
 	}
@@ -29,7 +72,8 @@ class Nav extends Component {
 Nav.contextTypes = {
 	lang: React.PropTypes.string,
 	messages: React.PropTypes.object,
-	langChange: React.PropTypes.func
+	langChange: React.PropTypes.func,
+	router: React.PropTypes.object
 };
 
 export default Nav;
