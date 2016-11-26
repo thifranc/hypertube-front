c5ee62791e8ee9c8a6ddee12ca2e300f9fae3416
mrdotb <baptiste.chaleil@gmail.com>
2016-11-26 15:08:00 +0100

    Fetch exemple

--- src/logged/Search.js
+++ src/logged/Search.js
@@ -1,6 +1,21 @@
 import React, {Component} from 'react';
+import 'whatwg-fetch';
 
 class Search extends Component {
+	componentDidMount() {
+		fetch('/api/v2/list_movies.json', {
+			method: 'GET',
+			credentials: 'include',
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		})
+			.then(res => res.json())
+			.then(res => {
+				console.log(res);
+			})
+			.catch(err => console.log(err));
+	}
 	render() {
 		return (
 			<div>Search</div>
