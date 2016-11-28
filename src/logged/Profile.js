715b0207debb0ff2a9ab395cf0a754d9bb2ab2c6
thifranc <thifranc@e3r12p21.42.fr>
2016-11-28 14:06:23 +0100

    file + lang in register + draft profile

--- src/logged/Profile.js
+++ src/logged/Profile.js
@@ -1,9 +1,39 @@
 import React, {Component} from 'react';
 
+import {List, ListItem} from 'material-ui/List';
+import Center from '../util/Center';
+
+const styles = {
+	img: {
+		height: '20%',
+		width: '20%',
+		display:'inline-block'
+	},
+	inline: {
+		display:'inline-block'
+	}
+};
+
 class Profile extends Component {
+	constructor(){
+		super();
+		this.img = 'http://www.filecluster.com/howto/wp-content/uploads/2014/07/User-Default.jpg';
+		this.name = 'default';
+		this.firstname = 'default';
+		this.login = 'default';
+		this.mail = 'default@gmail.com';
+	}
 	render() {
 		return (
-			<div>Profile</div>
+			<Center>
+				<img style={styles.img} src={this.img}/>
+				<List style={{display: 'inline-block'}}>
+				  <ListItem primaryText={this.login}/>
+				  <ListItem primaryText={this.name}/>
+				  <ListItem primaryText={this.firstname}/>
+				  <ListItem primaryText={this.mail}/>
+				</List>
+			</Center>
 		);
 	}
 }
