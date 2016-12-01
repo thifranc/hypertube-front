4ca78893cc50f7dc5f7306f800327e818e880f26
Baptiste CHALEIL <bchaleil@e3r11p11.42.fr>
2016-12-01 19:47:55 +0100

    Movie single in progress.

--- src/logged/Movie.js
+++ src/logged/Movie.js
@@ -1,12 +1,21 @@
 import React, {Component} from 'react';
+import Paper from 'material-ui/Paper';
 import CircularProgress from 'material-ui/CircularProgress';
 import FontIcon from 'material-ui/FontIcon';
 import Center from '../util/Center';
+import {List, ListItem} from 'material-ui/List';
+import ActionInfo from 'material-ui/svg-icons/action/info';
+import StarBorder from 'material-ui/svg-icons/toggle/star';
+import Eye from 'material-ui/svg-icons/image/remove-red-eye';
+import Play from 'material-ui/svg-icons/av/play-arrow';
+import IconButton from 'material-ui/IconButton';
+import Carousel from 'nuka-carousel';
+
+import {Table, TableBody, TableFooter, TableHeader, TableHeaderColumn, TableRow, TableRowColumn} from 'material-ui/Table';
+import TextField from 'material-ui/TextField';
 
 import 'whatwg-fetch';
 import './Movie.css';
-
-const star = <FontIcon color="yellow" className="material-icons">star_rate</FontIcon>;
 
 const styles = {
 	loader: {
@@ -45,20 +54,55 @@ class Movie extends Component {
 					<Center style={styles.loader}><CircularProgress size={80} thickness={5}/></Center> :
 					<div className="MovieContainer">
 						<div className="MovieMainColumn">
-							<div className="MovieMedia">
-								<img className="MovieMediaImage" src={movie.large_cover_image} alt="movie large cover image"/>
-							</div>
-							<div className="MovieInfo">
-								<h1>{movie.title}</h1>
-							</div>
+							<Paper zDepth={2}>
+								<div className="MovieMedia">
+									<Carousel wrapAround={Boolean(true)} autoplay={Boolean(true)}>
+										<img src={movie.large_screenshot_image1} alt="movie large cover image" onLoad={() => {window.dispatchEvent(new Event('resize'));}} />
+										<img src={movie.large_screenshot_image2} alt="movie large cover image" onLoad={() => {window.dispatchEvent(new Event('resize'));}} />
+										<img src={movie.large_screenshot_image3} alt="movie large cover image" onLoad={() => {window.dispatchEvent(new Event('resize'));}} />
+									</Carousel>
+								</div>
+								<div className="MovieInfo">
+									<h1>{movie.title}</h1>
+									<ul className="MovieInfoElemBlock">
+										<li className="MovieInfoElem">{movie.year}</li>
+										<li className="MovieInfoElem">{movie.mpa_rating}</li>
+										<li className="MovieInfoElem"><IconButton tooltip={movie.rating} touch={Boolean(true)} tooltipPosition="bottom-center"><StarBorder color="#f3e95a"/></IconButton></li>
+									</ul>
+									<p>{movie.description_full}</p>
+								</div>
+							</Paper>
 						</div>
 						<div className="MovieSecColumn">
-							<div className="MovieTorrentBlock">
-								<ul>
-									<li>Torrent</li>
-									<li>Torrent</li>
-								</ul>
-							</div>
+							<Paper zDepth={2}>
+								<div className="MovieTorrentBlock">
+									<Table>
+										<TableHeader>
+											<TableRow selectable={false}>
+												<TableHeaderColumn colSpan="5" tooltip="Torrents" style={{textAlign: 'center'}}>Torrents</TableHeaderColumn>
+											</TableRow>
+											<TableRow selectable={false}>
+												<TableHeaderColumn tooltip="Play">Play</TableHeaderColumn>
+												<TableHeaderColumn tooltip="Peers">Peers</TableHeaderColumn>
+												<TableHeaderColumn tooltip="Seeds">Seeds</TableHeaderColumn>
+												<TableHeaderColumn tooltip="Tracker">Tracker</TableHeaderColumn>
+												<TableHeaderColumn tooltip="Viewed">View</TableHeaderColumn>
+											</TableRow>
+										</TableHeader>
+										<TableBody>
+									{movie.torrents.map((torrent, index) => (
+										<TableRow key={index}>
+											<TableRowColumn><Play/></TableRowColumn>
+											<TableRowColumn>{torrent.peers}</TableRowColumn>
+											<TableRowColumn>{torrent.seeds}</TableRowColumn>
+											<TableRowColumn>Yts</TableRowColumn>
+											<TableRowColumn><Eye/></TableRowColumn>
+										</TableRow>
+										))}
+										</TableBody>
+									</Table>
+								</div>
+							</Paper>
 						</div>
 					</div>
 				}
