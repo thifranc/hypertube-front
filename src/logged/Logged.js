9b94a6147d3f352f1c1dd5eee9efdd20f66caf0a
thifranc <thifranc@e3r11p19.42.fr>
2016-12-12 15:56:54 +0100

    header sticky done

--- src/logged/Logged.js
+++ src/logged/Logged.js
@@ -2,7 +2,6 @@ import React, {Component} from 'react';
 import Nav from './Nav';
 
 import Footer from '../Footer';
-import './Logged.css';
 import { StickyContainer, Sticky } from 'react-sticky';
 
 class Logged extends Component {
@@ -15,16 +14,14 @@ class Logged extends Component {
 
 	render() {
 		return (
-			<div className="LoggedBlock">
-				<StickyContainer>
-				<Sticky>
-				<Nav path={this.props.location.pathname}/>
-				</Sticky>
+			<div>
+				<div style={{zIndex:"10", width:"100%", position:"fixed", top:"0"}}>
+					<Nav path={this.props.location.pathname}/>
+				</div>
 				{React.cloneElement(this.props.children, {token: this.state.token})}
-			<div style={{zIndex:"10", width:"100%", position:"fixed", bottom:"0"}}>
-				<Footer/>
-			</div>
-				</StickyContainer>
+				<div style={{zIndex:"10", width:"100%", position:"fixed", bottom:"0"}}>
+					<Footer/>
+				</div>
 			</div>
 		);
 	}
